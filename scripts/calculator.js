0b4e0f605bd698ce2f16ce8e07846d4d8928e219
Santiago Oviedo PC-Trabajo <oviedos412@gmail.com>
2024-08-16 13:20:44 -0300

    adding some functionality

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -25,13 +25,17 @@ const displayBottom = document.querySelector(".display__bottom span");
 const displayTop = document.querySelector(".display__top span");
 const buttons = document.querySelectorAll(".calculator__body button");
 
-const basicOperations = ['X', '+', '-', '/'];
+let result=undefined, isResult=false;
+
+const basicOperations = ['X', '+', '-', '/', '='];
+const numbers = ["0","1","2","3","4","5","6","7","8","9"];
 buttons.forEach(button =>{
     button.addEventListener("click", ()=>{
         let pressed = button.textContent;
-        if(displayBottom.textContent=="0" && !(basicOperations.includes(pressed))){ 
+        if((displayBottom.textContent=="0" && !(basicOperations.includes(pressed)) && pressed != ".") || (isResult && !(basicOperations.includes(pressed)))){ 
             displayBottom.textContent = !(pressed=="C") ? pressed : "0";
-            displayTop.textContent = (pressed=="C") ? "" : displayTop.textContent; 
+            displayTop.textContent = (pressed=="C") ? "" : displayTop.textContent;
+            isResult = false;
             return;
         }
         if(pressed=="C") {
@@ -39,11 +43,15 @@ buttons.forEach(button =>{
             return;
         }
         if(basicOperations.includes(pressed)){
+            if(displayTop.textContent.includes("=")) {
+                displayTop.textContent = ` ${result} ${pressed}`;
+                console.log(displayTop.textContent);
+                return;
+            }
             displayTop.textContent += ` ${displayBottom.textContent} ${pressed}`;
-            let result = eval(displayTop.textContent.toLowerCase());
-            console.log(result);
-            
-            displayBottom.textContent = Number(displayTop.textContent)
+            result = eval(displayTop.textContent.substring(0, (displayTop.textContent.length - 2)).toLowerCase().replace(/x/g, '*'));
+            isResult = true
+            displayBottom.textContent = result;
             return;
         }
         if(pressed=="( )"){
@@ -54,5 +62,8 @@ buttons.forEach(button =>{
             // if(displayBottom.textContent[displayBottom.textContent.lenght-1])
         }
         displayBottom.textContent += pressed[0];
+        if(pressed=="+/-"){
+            displayBottom.textContent = -parseFloat(displayBottom.textContent);
+        }
     });
 })
